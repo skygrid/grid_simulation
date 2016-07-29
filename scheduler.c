b583a48521a33a6ba66555ccb7d3262b4c7f2ba3
Ken <kenenbek>
2016-07-30 02:26:27 +0300

    imporoved scheduler

--- scheduler.c
+++ scheduler.c
@@ -7,41 +7,101 @@
 #include "messages.h"
 #include "myfunc_list.h"
 
-#define QUEUE_SIZE 100000
+#define QUEUE_SIZE 1000000
+jobPtr* matcher(long amountRequestedJob);
 
-int current_task_n = 0;
+long currentJobInQueue = 0;
+jobPtr* jobQueue;
 
 XBT_LOG_NEW_DEFAULT_CATEGORY(scheduler, "messages specific for scheduler");
 
+int scheduler(int argc, char* argv[]){
+    input();
+    char mailbox[30];
+    msg_task_t task = NULL;
+    sprintf(mailbox, "scheduler");
+    int id = (int) xbt_str_parse_int(argv[1], "Invalid argument %s");
+
+    while (1){
+        int res = MSG_task_receive(&task, mailbox);
+
+        // Anomalies
+        if (res == MSG_OK){
+        }else if (res == MSG_TRANSFER_FAILURE){
+            MSG_task_destroy(task);
+            task = NULL;
+        } else if (res == MSG_HOST_FAILURE){
+            MSG_task_destroy(task);
+            task = NULL;
+        }
+        if(!strcmp(MSG_task_get_name(task), "finalize")){
+            MSG_task_destroy(task);
+            break;
+        }
+
+        jobBatchRequestPtr batchRequest = MSG_task_get_data(task);
+        jobPtr* batch = matcher(batchRequest->coreAmount);
+
+        msg_error_t res1 = MSG_task_send(MSG_task_create("", batchRequest->coreAmount, MESSAGES_SIZE, batch),
+                                         MSG_host_get_name(MSG_task_get_source(task)));
+
+        // Anomalies
+        if (res1 == MSG_OK){
+        }else if (res1 == MSG_TRANSFER_FAILURE){
+            MSG_task_destroy(task);
+            task = NULL;
+        } else if (res1 == MSG_HOST_FAILURE){
+            MSG_task_destroy(task);
+            task = NULL;
+        }
+        if(!strcmp(MSG_task_get_name(task), "finalize")){
+            MSG_task_destroy(task);
+            break;
+        }
+
+
+    }
+}
+
+jobPtr* matcher(long amountRequestedJob){
+
+    jobPtr* jobBatch = xbt_new(jobPtr, amountRequestedJob);
+    for (long i = currentJobInQueue; i < (currentJobInQueue+amountRequestedJob); ++i) {
+        jobBatch[i-currentJobInQueue] = jobQueue[i];
+    }
+    return jobBatch;
+}
+
 int input(){
     int i = 0;
+    jobQueue = xbt_new(jobPtr, QUEUE_SIZE);
 
-    jobPtr* jobQueue = xbt_new(jobPtr, QUEUE_SIZE);
-
-    CsvParser *csvparser = CsvParser_new("data.csv", ",", 1);
+    CsvParser *csvparser = CsvParser_new("input.csv", ",", 1);
     CsvRow *header;
     CsvRow *row;
 
     while ((row = CsvParser_getRow(csvparser))){
         const char **rowFields = CsvParser_getFields(row);
+        jobPtr jobX = xbt_new(job, 1);
 
         //Parsing
-        jobQueue[i]->name = (char*) rowFields[0];
-        jobQueue[i]->type = charToEnum((char*) rowFields[1]);
-        jobQueue[i]->compSize = xbt_str_parse_double(rowFields[2], "error");
-        jobQueue[i]->inputFileName = (char*) rowFields[3];
-        jobQueue[i]->inputSize = xbt_str_parse_double(rowFields[4], "error");
-        jobQueue[i]->NRep = (int) xbt_str_parse_double(rowFields[5], "error");
-        jobQueue[i]->storageType = (char*) rowFields[6];
-        jobQueue[i]->dataLocHost1 = (char*) rowFields[7];
-        jobQueue[i]->dataLocHost2 = (char*) rowFields[8];
-        jobQueue[i]->dataLocHost3 = (char*) rowFields[9];
-        jobQueue[i]->outputName = (char*) rowFields[10];
-        jobQueue[i]->outputFileSize = xbt_str_parse_double(rowFields[11], "error");
-        jobQueue[i]->outputNumber = (int) xbt_str_parse_double(rowFields[12], "error");
-        jobQueue[i]->outputHost1 = (char*) rowFields[13];
-        jobQueue[i]->outputHost2 = (char*) rowFields[14];
-        jobQueue[i]->outputHost3 = (char*) rowFields[15];
+        jobX->name = strdup((char*) rowFields[0]);
+        jobX->type = charToEnum((char*) rowFields[1]);
+        jobX->compSize = xbt_str_parse_double(rowFields[2], "kotok1");
+        jobX->inputFileName = strdup((char*) rowFields[3]);
+        jobX->inputSize = xbt_str_parse_double(rowFields[4], "kotok2");
+        jobX->NRep = (int) xbt_str_parse_double(rowFields[5], "kotok3");
+        jobX->storageType = strdup((char*) rowFields[6]);
+        jobX->dataLocHost1 = strdup((char*) rowFields[7]);
+        jobX->dataLocHost2 = strdup((char*) rowFields[8]);
+        jobX->dataLocHost3 = strdup((char*) rowFields[9]);
+        jobX->outputName = strdup((char*) rowFields[10]);
+        jobX->outputFileSize = xbt_str_parse_double(rowFields[11], "kotok4");
+        jobX->outputNumber = (int) xbt_str_parse_double(rowFields[12], "kotok5");
+        jobX->outputHost1 = strdup((char*) rowFields[13]);
+        jobX->outputHost2 = strdup((char*) rowFields[14]);
+        jobX->outputHost3 = strdup((char*) rowFields[15]);
+        jobQueue[i] = jobX;
         CsvParser_destroy_row(row);
         i++;
     }
@@ -50,70 +110,6 @@ int input(){
 }
 
 
-int scheduler(int argc, char* argv[]){
-
-    MSG_process_set_kill_time(MSG_process_self(), 100.0);
-
-    int count = 0;
-    input();
-    char mailbox[30];
-    char destination[50];
-    msg_task_t task = NULL;
-    int id = (int) xbt_str_parse_int(argv[1], "Invalid argument %s");
-    sprintf(mailbox, "scheduler");
-
-    while (1){
-        if (count >= 100){
-            MSG_process_suspend(MSG_process_self());
-            break;
-        }
-        int res = MSG_task_receive(&task, mailbox);
-        XBT_INFO("Receive a pilot from %s", MSG_host_get_name(MSG_task_get_source(task)));
-
-        if(!strcmp(MSG_task_get_name(task), "finalize")){
-            MSG_task_destroy(task);
-            break;
-        }
-        // matching procedure; sending to dispatcher
-        sprintf(destination, "%s_%s", MSG_host_get_name(MSG_task_get_source(task)), "ST");
-        xbt_dynar_t dynar = MSG_task_get_data(task);
-        //xbt_dynar_sort(dynar, comparator);
-
-        xbt_dict_t map = match_task(&dynar);
-        XBT_INFO("Start send after matching to %s", destination);
-        msg_task_t taskB = MSG_task_create("", 0, MESSAGES_SIZE, map);
-        msg_error_t a = MSG_task_send(taskB, destination);
-
-        if (a == MSG_OK){
-            XBT_INFO("Send completed");
-        }
-        else if (a == MSG_HOST_FAILURE){
-            XBT_INFO("Problems with host");
-            free(map);
-            MSG_task_destroy(taskB);
-            return 0;
-        }
-        else if (a == MSG_TRANSFER_FAILURE){
-            XBT_INFO("Mmh. Something went wrong with '%s'. Nevermind. Let's keep going!", mailbox);
-            free(map);
-            MSG_task_destroy(taskB);
-        }
-        MSG_task_destroy(task);
-        task = NULL;
-        map = NULL;
-        count++;
-    }
-
-    return 0;
-}
-
-int kotok(){
-    int i; jobPtr j1;
-    jobPtr* jobBatchData = xbt_new(jobPtr , 10);
-    for (i = 0; i < 10; ++i) {
-        jobBatchData[i] = j1;
-    }
-}
 
 
 
--- str_to_enum.c
+++ str_to_enum.c
@@ -10,6 +10,6 @@ jobType charToEnum(char *sval)
     jobType result=USER;
     int i=0;
     for (i=0; typesStr[i]!=NULL; ++i, ++result)
-        if (0==strcmp(sval, typesStr[i])) return result;
+        if (0==strcasecmp(sval, typesStr[i])) return result;
     return UNKNOWN;
 }
