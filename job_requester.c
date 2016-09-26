0b581b44df9a58ed17fe8bdfd436a62c7aa7300b
Ken <kenenbek>
2016-09-26 16:49:10 +0300

    new outputs and better performance for 7 replicas due to decsreasing parameter of free-cores of job-requester

--- job_requester.c
+++ job_requester.c
@@ -18,7 +18,7 @@ int job_requester(){
     while (1){
         freeCoreAmount = fullCoreAmount - xbt_str_parse_int(MSG_host_get_property_value(MSG_host_self(), "activeCore"), "error") -
                 xbt_str_parse_int(MSG_host_get_property_value(MSG_host_self(), "corruptedCore"), "error");
-        if (freeCoreAmount > 4){
+        if (freeCoreAmount > 0){
             jobBatchRequestPtr jobRequest = xbt_new(jobBatchRequest, 1);
             jobRequest->coreAmount = freeCoreAmount;
 
