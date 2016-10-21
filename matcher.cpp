578c3bb4890bc37a40f4826c3981ab1e88a16afc
Ken <kenenbek>
2016-10-21 22:51:47 +0300

    удалил линкидлист

--- matcher.cpp
+++ matcher.cpp
@@ -127,3 +127,15 @@ vector<Job*>* matcher_DAM(long amountRequestedJob, const string host){
     return jobBatch;
 }
 
+
+list<Job*>* create_current_queue(){
+    std::list<Job*>* local_queue = new list<Job*>;
+
+    for (auto it = global_queue.begin(); it != global_queue.end(); ++it) {
+        if ((*it) && ((*it)->submissionTime < MSG_get_clock())){
+            local_queue->push_back(*it);
+        }else break;
+    }
+
+    return local_queue;
+}
