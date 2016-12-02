bf045b80f0640c370f8fef1a3865da09bde40445
Ken <kenenbek>
2016-12-02 23:14:09 +0300

    yaml convert

--- src/yaml.cpp
+++ src/yaml.cpp
@@ -2,4 +2,18 @@
 // Created by ken on 02.12.16.
 //
 
+#include <yaml-cpp/yaml.h>
 
+/*struct Job{
+
+};
+
+namespace YAML{
+    template <>
+
+    struct convert<Job>{
+        static bool decode(const Node& node, Job& job){
+
+        }
+    };
+}*/
