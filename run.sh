5206fa2ed3f39df35779a3a0199f16f7a7f269cc
Ken <kenenbek>
2016-12-27 23:10:21 +0300

    travis test 4

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ simple \
 --cfg=contexts/nthreads:1 \
 --cfg=tracing:yes \
 --cfg=tracing/platform:yes \
---cfg=tracing/filename:trash/trace \
+--cfg=tracing/filename:trace \
 --cfg=maxmin/concurrency_limit:200000 \
 --cfg=storage/max_file_descriptors:220000 \
